8417a4510a5cd5818daea272fd7090f6d4d89588
JorgeMagno <jorge.magnolm@gmail.com>
2022-04-06 00:42:59 -0300

    atualização do setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image-transform-rgb-gray",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Jorge Magno",
+    author_email="jorge.magnolm@gmail.com",
+    description="Pacote de transformação de imagens",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/JorgeMagno/image-processing-image-python",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',

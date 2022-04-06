48a14863669d5b1aa002a53fc60661ffccb049d9
JorgeMagno <jorge.magnolm@gmail.com>
2022-04-06 01:31:42 -0300

    atualização do setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_transform_rgbgray",
+    name="transform_rgbgray",
     version="0.0.1",
     author="Jorge Magno",
     author_email="jorge.magnolm@gmail.com",
